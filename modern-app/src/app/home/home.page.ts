87df7091d06952abb773ea31ac62892125a9300a
TouhidulHaider <touhidul_haider.me@protonmail.com>
2026-03-30 16:37:40 +0600

    set up API configuration

--- modern-app/src/app/home/home.page.ts
+++ modern-app/src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { MovieService } from '../services/movie';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  private movieService = inject(MovieService);
+
+  constructor() {
+    this.loadMovies();
+  }
+
+  loadMovies(){
+    this.movieService.getMovieDetails("1084187").subscribe((movies) => {
+      console.log(movies);
+    });
+  }
 }
--- modern-app/src/app/services/movie.ts
+++ modern-app/src/app/services/movie.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { ApiResult, MovieResult } from './interfaces';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = environment.apiKey;
@@ -10,17 +12,17 @@ const API_KEY = environment.apiKey;
   providedIn: 'root',
 })
 
-export class Movie {
+export class MovieService {
   private http = inject(HttpClient);
 
   constructor() {}
 
-  getTopRatedMovies(page = 1) {
-
+  getTopRatedMovies(page = 1): Observable<ApiResult> {
+    return this.http.get<ApiResult>(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`);
   }
   
-  getMovieDetails(id: string) {
-
+  getMovieDetails(id: string): Observable<MovieResult> {
+    return this.http.get<MovieResult>(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
   }
 
 }
